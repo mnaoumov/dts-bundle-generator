9f00415f48412b1b07906b773ae0c76ade81528c
Michael Naumov <mnaoumov@gmail.com>
2024-10-19 22:15:54 -0600

    Add test case

--- tests/e2e/test-cases/names-collision-with-globals/config.ts
+++ tests/e2e/test-cases/names-collision-with-globals/config.ts
@@ -1,6 +1,9 @@
 import { TestCaseConfig } from '../test-case-config';
 
 const config: TestCaseConfig = {
+    output: {
+        inlineDeclareExternals: true
+    }
 };
 
 export = config;
--- tests/e2e/test-cases/names-collision-with-globals/output.d.ts
+++ tests/e2e/test-cases/names-collision-with-globals/output.d.ts
@@ -1,3 +1,5 @@
+import { BaseEncodingOptions as Alias1, BigIntOptions as Alias2 } from 'node:fs';
+
 interface Promise$3 {
 }
 interface Date$2 {
@@ -7,6 +9,18 @@ export interface Int {
 	globalD: Date;
 	localP: Promise$3;
 	globalP: Promise<number>;
+}
+export interface Interface1 extends Alias1 {
+}
+export interface Interface2 extends Alias2 {
+}
+declare module "node:fs" {
+	interface BaseEncodingOptions {
+		newField: string;
+	}
+	class BigIntOptions {
+		newField: string;
+	}
 }
 
 export {};
